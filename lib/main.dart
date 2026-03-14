8118d03c36ee66e60bb0d72ae56e54adb827c8d0
Freewind <nowindlee@gmail.com>
2026-03-14 22:11:15 +0800

    flutter-graphql-demo

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+// Flutter GraphQL
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('GraphQL Demo')),
+        body: Center(
+          child: Text('Use graphql_flutter package'),
+        ),
+      ),
+    );
+  }
+}
